ae8d6bedc5def316bd5d9f058843ca78dc17106d
dobbinsi <iandobbins5@gmail.com>
2022-07-20 15:15:14 -0700

    get paid in usdc

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeadComponent from "../components/Head";
 import { PublicKey } from "@solana/web3.js";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Product from "../components/Product";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -11,29 +12,52 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const App = () => {
   // This will fetch the users' public key (wallet address) from any wallet we support
   const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
 
   const renderNotConnectedContainer = () => (
     <div>
       <img
-        src="https://media.giphy.com/media/RaTAFdOkxs0X6/giphy.gif"
+        src="https://media.giphy.com/media/10SrYA07iDPyO4/giphy.gif"
         alt="emoji"
+        className="main-gif"
       />
-
       <div className="button-container">
         <WalletMultiButton className="cta-button connect-wallet-button" />
       </div>
     </div>
   );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
       <HeadComponent />
       <div className="container">
         <header className="header-container">
-          <p className="header"> InUrFeelz Emoji </p>
+          <p className="header">Proof of Noodle 🍜</p>
           <p className="sub-text">Now accepting Solana Pay!</p>
         </header>
 
-        <main>{publicKey ? "Connected!" : renderNotConnectedContainer()}</main>
+        <main>
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
 
         <div className="footer-container">
           <img
